849d4624eee1b283d9b2428bf08791c10ea16d3d
coryhelm <cory_17h@hotmail.com>
2024-03-06 12:18:16 -0500

    added .evn file and adjusted firebase config

--- mobile-app/firebaseConfig.ts
+++ mobile-app/firebaseConfig.ts
@@ -4,14 +4,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig: FirebaseOptions = {
-  apiKey: "REACT_GPS_API_KEY",
-  authDomain: "REACT_APP_AUTH_DOMAIN",
-  databaseURL: "REACT_DATABASE_URL",
-  projectId: "REACT_PROJECT_ID",
-  storageBucket: "REACT_STORAGE_BUCKET",
-  messagingSenderId: "REACT_MESSAGING_SENDER_ID",
-  appId: "REACT_APP_ID",
-  measurementId: "REACT_MEASUREMENT_ID",
+  apiKey: "${process.env.REACT_GPS_API_KEY}",
+  authDomain: "${process.env.REACT_APP_AUTH_DOMAIN}",
+  databaseURL: "${process.env.REACT_DATABASE_URL}",
+  projectId: "${process.env.REACT_PROJECT_ID}",
+  storageBucket: "${process.env.REACT_STORAGE_BUCKET}",
+  messagingSenderId: "${process.env.REACT_MESSAGING_SENDER_ID}",
+  appId: "${process.env.REACT_APP_ID}",
+  measurementId: "${process.env.REACT_MEASUREMENT_ID}",
 };
 
 export const FIREBASE_APP = initializeApp(firebaseConfig);
