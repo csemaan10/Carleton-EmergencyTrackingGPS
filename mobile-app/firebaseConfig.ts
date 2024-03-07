c48dd4ba0cff10600273121db6275725b178b703
Anthony Massaad <massaadanthony@hotmail.com>
2024-03-07 10:12:04 -0500

    fix configuration for .env file

--- mobile-app/firebaseConfig.ts
+++ mobile-app/firebaseConfig.ts
@@ -4,14 +4,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig: FirebaseOptions = {
-  apiKey: `${process.env.REACT_GPS_API_KEY}`,
-  authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
-  databaseURL: `${process.env.REACT_DATABASE_URL}`,
-  projectId: `${process.env.REACT_PROJECT_ID}`,
-  storageBucket: `${process.env.REACT_STORAGE_BUCKET}`,
-  messagingSenderId: `${process.env.REACT_MESSAGING_SENDER_ID}`,
-  appId: `${process.env.REACT_APP_ID}`,
-  measurementId: `${process.env.REACT_MEASUREMENT_ID}`
+  apiKey: `${process.env.EXPO_PUBLIC_FIREBASE_API_KEY}`,
+  authDomain: `${process.env.EXPO_PUBLIC_FIREBASE_APP_AUTH_DOMAIN}`,
+  databaseURL: `${process.env.EXPO_PUBLIC_FIREBASE_DATABASE_URL}`,
+  projectId: `${process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID}`,
+  storageBucket: `${process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID}`,
+  appId: `${process.env.EXPO_PUBLIC_FIREBASE_APP_ID}`,
+  measurementId: `${process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID}`,
 };
 
 export const FIREBASE_APP = initializeApp(firebaseConfig);
